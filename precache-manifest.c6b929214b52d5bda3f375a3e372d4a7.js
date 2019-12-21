4f8b98af7d9fa1fc638694761c40e9feebcca46e
Michaela Wagner <mickywagner@gmail.com>
2019-12-20 20:41:12 -0700

    Updates

---
+++ precache-manifest.c6b929214b52d5bda3f375a3e372d4a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b93b29b3408020169b054d3a0749c0",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "4f41794f4d453d98e182",
+    "url": "/react-todo/static/css/main.994ae876.chunk.css"
+  },
+  {
+    "revision": "3ec9eb97c8e8a9a16a65",
+    "url": "/react-todo/static/js/2.2714d2b9.chunk.js"
+  },
+  {
+    "revision": "4f41794f4d453d98e182",
+    "url": "/react-todo/static/js/main.b2791960.chunk.js"
+  },
+  {
+    "revision": "599788696c3385f97903",
+    "url": "/react-todo/static/js/runtime-main.80da223f.js"
+  }
+]);
