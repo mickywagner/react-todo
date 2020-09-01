a74680195a5b89afa2a14131ac89d20c1809ea44
Michaela Wagner <mickywagner@gmail.com>
2020-09-01 15:44:38 -0600

    Updates

---
+++ precache-manifest.4b383ca7908505db069cc1abfe3902cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e239c48d388560365f80e887d4dcd76",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "a4dee0d3223c6d684bcc",
+    "url": "/react-todo/static/css/main.03950af8.chunk.css"
+  },
+  {
+    "revision": "78be7d98fb62cbf4e30d",
+    "url": "/react-todo/static/js/2.0616204d.chunk.js"
+  },
+  {
+    "revision": "a4dee0d3223c6d684bcc",
+    "url": "/react-todo/static/js/main.de6a46e8.chunk.js"
+  },
+  {
+    "revision": "f9e17421f7db639456ef",
+    "url": "/react-todo/static/js/runtime-main.9a64aca9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-todo/precache-manifest.c6b929214b52d5bda3f375a3e372d4a7.js"
+  "/react-todo/precache-manifest.4b383ca7908505db069cc1abfe3902cf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-todo/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
